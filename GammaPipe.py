dbb0b08a30dc61bab2d022273e790d8702ef8604
Andrea Bulgarelli <bulgarelli@iasfbo.inaf.it>
2017-12-27 18:59:42 +0100

    new

--- GammaPipe.py
+++ GammaPipe.py
@@ -25,31 +25,40 @@ class GammaPipe:
 		#in_tstart
 		#in_duration
 		#in_obsid
-		
+
 		#From target:
 		#in_emin
 		#in_emax
-		
+
 		#From instrument
 		#in_fov
 		#in_irf
 		#in_caldb
 		
-		
+		self.in_ra                   =   83.63
+		self.in_dec                  =   22.01
+		self.in_fov              =   10.0
+		#rad_select           =    3.0
+		self.in_tstart  =    0.0
+		self.in_duration =  300.0
+		self.in_emin                 =    0.1
+		self.in_emax                 =  100.0
+
+
 		if in_pnttype == 'celestial' :
 			pntdir.radec_deg(in_ra, in_dec)
-			
+	
 		if in_pnttype == 'equatorial' :
 			pntdir.radec_deg(in_ra, in_dec)
-		
+
 		#if in_pnttype == 'galactic' :
 		#	pntdir.radec_deg(in_l, in_b)
-		
+
 		obs = obsutils.set_obs(pntdir, in_tstart, in_duration, 1.0, \
-            in_emin, in_emax, in_fov, \
-            in_irf, in_caldb, in_obsid)
-            
-        return obs
+			in_emin, in_emax, in_fov, \
+			in_irf, in_caldb, in_obsid)
+	
+		return obs
 
 	def run_pipeline(self, obs, enumbins=1, nxpix=200, nypix=200, binsz=0.02):
 		"""
@@ -59,73 +68,59 @@ class GammaPipe:
 		events_name          = 'events.fits'
 		selected_events_name = 'selected_events.fits'
 		result_name          = 'results.xml'
-		ra                   =   83.63
-		dec                  =   22.01
-		rad_sim              =   10.0
-		rad_select           =    3.0
-		tstart               =    0.0
-		tstop                =  300.0
-		emin                 =    0.1
-		emax                 =  100.0
+		
 
 		# Simulate events
-		sim = ctools.ctobssim()
+		sim = ctools.ctobssim(obs)
 		sim['inmodel']   = self._model
 		sim['outevents'] = events_name
 		sim['caldb']     = self._caldb
 		sim['irf']       = self._irf
-		sim['ra']        = ra
-		sim['dec']       = dec
-		sim['rad']       = rad_sim
-		sim['tmin']      = tstart
-		sim['tmax']      = tstop
-		sim['emin']      = emin
-		sim['emax']      = emax
 		sim.execute()
-		
-		
-		bin = ctools.ctbin()
-        bin['inobs']    = eventfile
-        bin['outcube']  = cubefile
-        bin['ebinalg']  = 'LOG'
-        bin['emin']     = emin
-        bin['emax']     = emax
-        bin['enumbins'] = enumbins
-        bin['nxpix']    = nxpix
-        bin['nypix']    = nypix
-        bin['binsz']    = binsz
-        bin['coordsys'] = coordsys
-        bin['usepnt']   = True
-        bin['proj']     = proj
-        bin.execute()
-        
-        # Set observation ID
-        bin.obs()[0].id(cubefile)
-        bin.obs()[0].eventfile(cubefile)
 
-		
+
+		bin = ctools.ctbin(obs)
+		bin['inobs']    = eventfile
+		bin['outcube']  = cubefile
+		bin['ebinalg']  = 'LOG'
+		bin['emin']     = emin
+		bin['emax']     = emax
+		bin['enumbins'] = enumbins
+		bin['nxpix']    = nxpix
+		bin['nypix']    = nypix
+		bin['binsz']    = binsz
+		bin['coordsys'] = coordsys
+		bin['usepnt']   = True # Use pointing for map centre
+		bin['proj']     = proj
+		bin.execute()
+
+		# Set observation ID
+		bin.obs()[0].id(cubefile)
+		bin.obs()[0].eventfile(cubefile)
+
+
 
 		# Select events
 		# select = ctools.ctselect()
-# 		select['inobs']  = events_name
-# 		select['outobs'] = selected_events_name
-# 		select['ra']     = ra
-# 		select['dec']    = dec
-# 		select['rad']    = rad_select
-# 		select['tmin']   = tstart
-# 		select['tmax']   = tstop
-# 		select['emin']   = emin
-# 		select['emax']   = emax
-# 		select.execute()
+		# 		select['inobs']  = events_name
+		# 		select['outobs'] = selected_events_name
+		# 		select['ra']     = ra
+		# 		select['dec']    = dec
+		# 		select['rad']    = rad_select
+		# 		select['tmin']   = tstart
+		# 		select['tmax']   = tstop
+		# 		select['emin']   = emin
+		# 		select['emax']   = emax
+		# 		select.execute()
 
 		# Perform maximum likelihood fitting
-# 		like = ctools.ctlike()
-# 		like['inobs']    = selected_events_name
-# 		like['inmodel']  = self._model
-# 		like['outmodel'] = result_name
-# 		like['caldb']    = self._caldb
-# 		like['irf']      = self._irf
-# 		like.execute()
+		# 		like = ctools.ctlike()
+		# 		like['inobs']    = selected_events_name
+		# 		like['inmodel']  = self._model
+		# 		like['outmodel'] = result_name
+		# 		like['caldb']    = self._caldb
+		# 		like['irf']      = self._irf
+		# 		like.execute()
 
 		# Return
 		return
--- Pipe1.py
+++ Pipe1.py
@@ -9,38 +9,43 @@ import GammaPipe
 # Run binned in-memory pipeline #
 # ============================= #
 def pipeline_binned():
-    """
-    Run binned pipeline
-    """
-    # Set usage string
-    usage = 'Pipe1.py [-d datadir] [-s simmodel] [-a analysismodel] [-c configuration pipe]'
+	print('Run binned pipeline')
+	"""
+	Run binned pipeline
+	"""
+	# Set usage string
+	usage = 'Pipe1.py [-d datadir] [-s simmodel] [-a analysismodel] [-c configuration pipe]'
 
-    # Set default options
-    options = [{'option': '-o', 'obs': 'observation'}, {'option': '-s', 'sim': 'simmodel'}, {'option': '-a', 'an': 'analysismodel'}, {'option': '-c', 'conf': 'confpipe'}]
-    
+	# Set default options
+	options = [{'option': '-observation', 'value': ''}, {'option': '-simmodel', '': ''}, {'option': '-anamodel', 'value': 'crab.xml'}, {'option': '-confpipe', 'value': ''}]
 
-    # Get arguments and options from command line arguments
-    args, options = cscripts.ioutils.get_args_options(options, usage)
+	# Get arguments and options from command line arguments
+	args, options = cscripts.ioutils.get_args_options(options, usage)
 
-    # Extract script parameters from options
-    obsfilename = options[0]['obs']
-    simfilename = options[0]['sim']
-    analysisfilename = options[0]['an']
-    conffilename = options[0]['conf']
-    
-    gp = GammaPipe()
-    
-    # Setup observations
-    obs = gp.open_observation(obsfilename)
-    
-    # Setup simulation model
-    obs.models(gammalib.GModels(simfilename))
+	# Extract script parameters from options
+	obsfilename = options[0]['value']
+	simfilename = options[1]['value']
+	analysisfilename = options[2]['value']
+	conffilename = options[3]['value']
 
-    # Run analysis pipeline
-    gp.run_pipeline(obs, enumbins=1, nxpix=200, nypix=200, binsz=0.02)
+	print(obsfilename)
+	print(simfilename)
+	print(analysisfilename)
+	print(conffilename)
 
-    # Return
-    return
+	gp = GammaPipe.GammaPipe()
+
+	# Setup observations
+	obs = gp.open_observation(obsfilename)
+
+	# Setup simulation model
+	obs.models(gammalib.GModels(simfilename))
+
+	# Run analysis pipeline
+	gp.run_pipeline(obs, enumbins=1, nxpix=200, nypix=200, binsz=0.02)
+
+	# Return
+	return
 
 
 # ======================== #
