cf52dc0d838d14d673953c2c414b7563840f0b39
sim <simotrone@gmail.com>
2019-04-22 16:07:14 +0200

    execute logic in ExecuteCTools only

--- ExecuteCTools.py
+++ ExecuteCTools.py
@@ -17,106 +17,94 @@
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #
 # ==========================================================================
-
-
-from datetime import datetime
-
-print("before import")
-print(datetime.utcnow().strftime('%Y-%m-%d %H:%M:%S.%f')[:-3])
-
-import os
-import shutil
 import cscripts
-import DetectionAndMaps
-import DetectionOnOff
+import logging
+import Detection
 from PipeConfiguration import CToolsRunConfiguration
 from PipeConfiguration import PostAnalysisCopyFilesConfiguration
 from PostAnalysis import *
 
+logging.basicConfig(level=logging.DEBUG, format='%(asctime)s:%(levelname)s:%(message)s',)
 
-print("after import")
-print(datetime.utcnow().strftime('%Y-%m-%d %H:%M:%S.%f')[:-3])
+def run_pipeline():
+	"""
+	Run pipeline
+	"""
+	params = init_params()
+	logging.info("before run pipe")
+	if params['postanalysis']:
+		executePostAnalysis(params['postanalysis'])
+	else:
+		executeDetection(params)
+	logging.info("after run pipe")
+	return
 
-
+def init_params():
+	usage = 'ExecuteCTools.py [-observation obsfilename] [-simmodel simmodelfilename] [-anamodel analysismodelfilename] [-confpipe configuration pipe][-seed seed]'
+	options = [
+			{'option': '-observation', 'value': ''},
+			{'option': '-simmodel',      'value': ''},
+			{'option': '-anamodel',      'value': ''},
+			{'option': '-runconf',       'value': ''},
+			{'option': '-eventfilename', 'value': ''},
+			{'option': '-seed',          'value': '0'},
+			{'option': '-postanalysis',  'value': ''}
+	]
+	# Get arguments and options from command line arguments
+	args, options = cscripts.ioutils.get_args_options(options, usage)
+	# logging.debug('args: %s' % args)
+	# logging.debug('options: %s' % options)
+	# Extract script parameters from options
+	params = {
+			'obsfilename':      options[0]['value'],
+			'simfilename':      options[1]['value'],
+			'analysisfilename': options[2]['value'],
+			'runconffilename':  options[3]['value'],
+			'eventfilename':    options[4]['value'],
+			'in_seed':          int(options[5]['value']),
+			'postanalysis':     options[6]['value']
+	}
+	logging.debug('params: %s' % params)
+	return params
 
 def executePostAnalysis(filename):
-	# Setup postanalysis
-	if filename:
+	"""
+	setup and execute post analysis
+	"""
+	logging.debug('execute post-analysis on file "%s"', filename)
+	try:
 		sessionconf = PostAnalysisCopyFilesConfiguration(filename)
+	except Exception as e:
+		raise e
 
 	if sessionconf.postanalysis == "copyfiles":
 		copyfiles = PostAnalysisCopyFiles(sessionconf)
 		copyfiles.execute()
-
 	merge_results = PostAnalysisMergeResults(sessionconf)
 	merge_results.execute()
 
-def pipeline_binned():
-	print('Run binned pipeline')
+def executeDetection(params):
 	"""
-	Run binned pipeline
+	setup and execute detection
 	"""
-	# Set usage string
-	usage = 'ExecuteCTools.py [-observation obsfilename] [-simmodel simmodelfilename] [-anamodel analysismodelfilename] [-confpipe configuration pipe][-seed seed]'
-
-	# Set default options
-	options = [{'option': '-observation', 'value': ''}, {'option': '-simmodel', 'value': ''}, {'option': '-anamodel', 'value': ''}, {'option': '-runconf', 'value': ''}, {'option': '-eventfilename', 'value': ''}, {'option': '-seed', 'value': '0'}, {'option': '-postanalysis', 'value': ''},]
-
-	# Get arguments and options from command line arguments
-	args, options = cscripts.ioutils.get_args_options(options, usage)
-
-	# Extract script parameters from options
-	obsfilename = options[0]['value']
-	simfilename = options[1]['value']
-	analysisfilename = options[2]['value']
-	runconffilename = options[3]['value']
-	eventfilename = options[4]['value']
-	in_seed = int(options[5]['value'])
-	postanalysis = options[6]['value']
-
-	print('obsfilename: ' + obsfilename)
-	print('simfilename: ' + simfilename)
-	print('analysisfilename: ' + analysisfilename)
-	print('runconffilename: ' + runconffilename)
-	print('eventfilename: ' + eventfilename)
-	print('postanalysis: ' + postanalysis)
-
-	print("before run pipe")
-	print(datetime.utcnow().strftime('%Y-%m-%d %H:%M:%S.%f')[:-3])
-
-	if not postanalysis:
-		runconf = CToolsRunConfiguration(runconffilename)
-
-		print("not postanalysis not results_xml")
-		if runconf.onoff_analysis == 1:
-			print("starting on/off analysis")
-			gp = DetectionOnOff.DetectionOnOff()
-		else:
-			print("starting detection and maps")
-			gp = DetectionAndMaps.DetectionAndMaps()
-
-		gp.init(obsfilename, simfilename, analysisfilename, runconf, eventfilename)
-		print("after init pipe")
-		print(datetime.utcnow().strftime('%Y-%m-%d %H:%M:%S.%f')[:-3])
-
-		# Run analysis pipeline
-		gp.run_pipeline(seed=in_seed)
-
-		print("after run pipe")
-		print(datetime.utcnow().strftime('%Y-%m-%d %H:%M:%S.%f')[:-3])
-
-	if postanalysis:
-
-		executePostAnalysis(postanalysis)
-
-	# Return
+	logging.debug('runconf file: %s' % params['runconffilename'])
+	params['runconf'] = CToolsRunConfiguration(params['runconffilename'])
+	logging.debug('params[runconf]: %s' % params['runconf'])
+	exit(0)
+	gp = Detection.Detection(params) # obsfilename, simfilename, analysisfilename, runconf, eventfilename)
+	gp.run_pipeline(seed=params['in_seed'])
+	# if runconf.onoff_analysis == 1:
+	# 	print("starting on/off analysis")
+	# 	gp = DetectionOnOff.DetectionOnOff()
+	# else:
+	# 	print("starting detection and maps")
+	# 	gp = DetectionAndMaps.DetectionAndMaps()
+	# gp.init(obsfilename, simfilename, analysisfilename, runconf, eventfilename)
+	# gp.run_pipeline(seed=in_seed)
 	return
-
 
 # ======================== #
 # Main routine entry point #
 # ======================== #
 if __name__ == '__main__':
-
-    # Run binned in-memory pipeline
-    pipeline_binned()
+    run_pipeline()
